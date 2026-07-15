629c2afc8149801032e0529cbee2a66e64449179
Melon Ask <alexboltafy@gmail.com>
2026-07-15 23:15:07 +0300

    Update crudo to 0.1.1

--- Formula/crudo.rb
+++ Formula/crudo.rb
@@ -1,8 +1,8 @@
 class Crudo < Formula
   desc "Configuration-driven JSON APIs backed by SQL"
   homepage "https://github.com/melonask/crudo"
-  url "https://github.com/melonask/crudo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "df0c5342fdcadb416f6529410f852717aa9a1270c3bffd08cccae6385530721d"
+  url "https://github.com/melonask/crudo/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "e02cadf1ecda386a0831d792e18ab88045e74b8bb3eebbbd0c2ed4feaa548a16"
   license "MIT"
   head "https://github.com/melonask/crudo.git", branch: "main"
 
@@ -13,6 +13,6 @@ class Crudo < Formula
   end
 
   test do
-    assert_match "crudo 0.1.0", shell_output("#{bin}/crudo --version")
+    assert_match "crudo 0.1.1", shell_output("#{bin}/crudo --version")
   end
 end
